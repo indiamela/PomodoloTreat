505c379fd991db9c06ce921b8d7369bab3c61fcd
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-05-03 09:43:09 +0900

    add-todoCard

--- PomodoloTreat/HomeView.swift
+++ PomodoloTreat/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
         ZStack{
             Color.MyTheme.whiteColor
                  .edgesIgnoringSafeArea(.all)
+            //MARK:TIMER -
             VStack{
                 ZStack{
                     //Rectangle
@@ -28,12 +29,17 @@ struct HomeView: View {
                     
                     VStack{
                         HStack{
-                            Image(systemName: "house")
+                            Image(systemName: "text.justify")
                                 .resizable()
-                                .frame(width: 30, height: 30)
+                                .scaledToFit()
+                                .frame(width: 20)
+                                .foregroundColor(Color.MyTheme.whiteColor)
                             Spacer()
-                            Image(systemName: "house")                                .resizable()
-                                .frame(width: 30, height: 30)
+                            Image(systemName: "square.and.arrow.up")                                .resizable()
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                                .foregroundColor(Color.MyTheme.whiteColor)
                         }
                         .padding(.horizontal,30)
                         Spacer()
@@ -56,49 +62,47 @@ struct HomeView: View {
                     }
                 }
                 .frame(height:300)
-                ScrollView{
-                    ZStack{
-                        Rectangle()
-                            .foregroundColor(Color.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                        HStack{
-                            Text("Placeholder")
-                            Spacer()
-                            VStack{
+                
+                //MARK:LIST -
+                ScrollView{                    
+                    //Box
+                    VStack{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .shadow(color: Color(#colorLiteral(red: 0.46666666865348816, green: 0.7333333492279053, blue: 0.9333333373069763, alpha: 0.25)), radius:10, x:10, y:10)
+                                .overlay(
+                                    HStack{
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .fill(Color.MyTheme.yellowColor)
+                                            .frame(width: 6)
+                                        Spacer()
+                                    }
+                                )
+                            HStack{
                                 HStack{
-                                    Image(systemName: "smile")
-                                    Text("70%")
+                                    Text("1")
+                                        .font(.title3)
+                                    Text("Placeholder")
+                                        .multilineTextAlignment(.leading)
                                 }
-                                Text("12:00 - 12:25")
+                                Spacer()
+                                VStack(alignment:.trailing){
+                                    HStack{
+                                        Image(systemName: "smile")
+                                        Text("70%")
+                                    }
+                                    Text("12:00-12:25")
+                                }
+                                .frame(width: 100)
                             }
-
+                            .frame(height:30)
+                            .padding()
                         }
-                        .padding(10)
+                        .frame(width: 327)
+                        .padding(.init(top: 0, leading: 20, bottom: 20, trailing: 20))
                     }
-                    .frame(width: 300, alignment: .center)
                     .padding()
-                    
-                    ZStack{
-                        Rectangle()
-                            .foregroundColor(Color.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                        HStack{
-                            Text("Placeholder")
-                            Spacer()
-                            VStack{
-                                HStack{
-                                    Image(systemName: "smile")
-                                    Text("70%")
-                                }
-                                Text("12:00 - 12:25")
-                            }
-
-                        }
-                        .padding(10)
-                    }
-                    .frame(width: 300, alignment: .center)
                 }
             }
         }
